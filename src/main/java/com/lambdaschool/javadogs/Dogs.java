2e7873066c7628c452b577de00a472046bb0b14e
Jacob Seppanen <jbseppanen@gmail.com>
2019-02-12 11:58:17 -0800

    Add dogs class.

---
+++ src/main/java/com/lambdaschool/javadogs/Dogs.java
@@ -0,0 +1,27 @@
+package com.lambdaschool.javadogs;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data //Creates getters, setters, toString
+@Entity //Object ready for JPA storage
+public class Dogs {
+    private @Id @GeneratedValue Long id;
+    String breed;
+    int weight;
+    boolean apartmentOK;
+
+
+    public Dogs(String breed, int weight, boolean apartmentOK) {
+        this.breed = breed;
+        this.weight = weight;
+        this.apartmentOK = apartmentOK;
+    }
+
+    public Dogs() {
+    }
+
+}
