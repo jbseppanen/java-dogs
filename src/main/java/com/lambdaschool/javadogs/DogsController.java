953b707ad426986f9b93add35b79a6e7b3c76ab2
Jacob Seppanen <jbseppanen@gmail.com>
2019-02-12 12:16:46 -0800

    Add DogsResourceAssembler class.

---
+++ src/main/java/com/lambdaschool/javadogs/DogsController.java
@@ -0,0 +1,11 @@
+package com.lambdaschool.javadogs;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DogsController {
+
+    private final DogsRepository dogsRepo;
+    private final
+
+}
